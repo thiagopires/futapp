c9884bdd325fe865b1461e2bf112d379f919764f
Thiago <thiago.serip@gmail.com>
2024-12-19 13:37:31 -0300

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -8,11 +8,32 @@ st.sidebar.header("Leagues")
 selected_league = st.sidebar.selectbox('League', ['England','Germany','Italy','Spain','France'])
 
 st.sidebar.header("Season")
-selected_season = st.sidebar.selectbox('Season', ['2025/2024','2024/2023','2023/2022','2022/2021','2021/2020'])
+selected_season = st.sidebar.selectbox('Season', ['2024/2025','2023/2024','2022/2023','2021/2022','2020/2021'])
 
 def load_data(league, season):
+
+  match league:
+    case 'England':
+      league = 'E0'
+    case 'Germany':
+      league = 'D1'
+    case 'Italy':
+      league = 'I1'
+    case 'Spain':
+      league = 'SP1'
+    case 'France':
+      league = 'F1'
+
+  season = season[2,2]+season[7,2]
+  
   url = f"https://www.football-data.co.uk/mmz4281/{season}/{league}.csv"
   data = pd.read_csv(url)
   return data
 
 df = load_data(selected_league, selected_season)
+
+st.subheader(f"Dataframe: {selected_league) - {selected_season}")
+
+st.dataframe(df)
+
+st.dataframe(df
