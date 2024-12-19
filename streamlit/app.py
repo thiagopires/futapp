3d04fbf58c1a509c69e7783001eafe9c6774bc68
Thiago <thiago.serip@gmail.com>
2024-12-19 13:57:18 -0300

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -4,12 +4,11 @@ import numpy as np
 
 st.title("Web App Football Data")
 
-st.sidebar.header("Leagues")
-selected_league = st.sidebar.selectbox('League', ['England','Germany','Italy','Spain','France'])
+st.sidebar.header("Filtros")
+selected_league = st.sidebar.selectbox('Escolha uma liga', ['England','Germany','Italy','Spain','France'])
+selected_season = st.sidebar.selectbox('Escolha uma temporada', ['2024/2025','2023/2024','2022/2023','2021/2022','2020/2021'])
 
-st.sidebar.header("Season")
-selected_season = st.sidebar.selectbox('Season', ['2024/2025','2023/2024','2022/2023','2021/2022','2020/2021'])
-
+@st.cache_data
 def load_data(league, season):
 
   match league:
@@ -24,9 +23,7 @@ def load_data(league, season):
     case 'France':
       league = 'F1'
 
-  season = season[2:4]+season[7:9]
-  
-  url = f"https://www.football-data.co.uk/mmz4281/{season}/{league}.csv"
+  url = f"https://www.football-data.co.uk/mmz4281/{season[2:4]}{season[7:9]}/{league}.csv"
   data = pd.read_csv(url)
   return data
 
