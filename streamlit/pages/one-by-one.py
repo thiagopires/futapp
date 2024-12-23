f06a77e0e21a49a06c6133a0a488b7a3d281c90c
Thiago Pires <thiago.serip@gmail.com>
2024-12-23 11:43:59 -0300

    .

--- streamlit/pages/one-by-one.py
+++ streamlit/pages/one-by-one.py
@@ -77,8 +77,7 @@ def generate_classificacao(df, type):
     elif type == 'AWAY':
         classificacao = classificacao.style.apply(lambda v: 'background-color: yellow' if v == df_match_selected["Away"] else '', subset=["Clube"])
     elif type == 'ALL':
-        classificacao = classificacao.style.apply(lambda v: 'background-color: yellow' if v == df_match_selected["Home"] else '', subset=["Clube"])
-        classificacao = classificacao.style.apply(lambda v: 'background-color: yellow' if v == df_match_selected["Away"] else '', subset=["Clube"])
+        classificacao = classificacao.style.apply(lambda v: 'background-color: yellow' if v == df_match_selected["Home"] or v == df_match_selected["Away"] else '', subset=["Clube"])
 
     return classificacao
 
