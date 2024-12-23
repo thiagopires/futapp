b1ded525c76f9ead0e351b2df2caddbdcfcc8cd4
Thiago <thiago.serip@gmail.com>
2024-12-23 00:35:30 -0300

    Update one-by-one.py

--- streamlit/pages/one-by-one.py
+++ streamlit/pages/one-by-one.py
@@ -66,7 +66,7 @@ st.divider()
 #     "Resultado": ["1-2", "0-3", "2-1"]
 # })
 filter_confrontos = (df_hist["Home"].isin([df_match_selected["Home"], df_match_selected["Away"]])) & (df_hist["Away"].isin([df_match_selected["Home"], df_match_selected["Away"]]))
-confrontos = df_hist.loc[filter_confrontos, ["Date", "Season", "Home", "Away"]].sort_values(by="Date", ascending=False)
+confrontos = df_hist.loc[filter_confrontos, ["Date", "Season", "Home", "Resultado_FT", "Away"]].sort_values(by="Date", ascending=False)
 
 # Dividindo a página em duas colunas
 col1, col2 = st.columns(2)
