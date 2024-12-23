8d9bd3e5966a4737975334c01c2c0e91d52f79d1
Thiago Pires <thiago.serip@gmail.com>
2024-12-23 17:35:17 -0300

    .

--- streamlit/pages/one-by-one.py
+++ streamlit/pages/one-by-one.py
@@ -154,10 +154,10 @@ if middle.button("Amanhã", use_container_width=True):
 if right.button("D.Amanhã", use_container_width=True):
     df_matches = load_daymatches(2)
 
-df_matches["Confronto"] = df_matches["Home"] + " vs. " + df_matches["Away"]
+df_matches["Confronto"] = df_matches["Time"] + " - " + df_matches["Home"] + " vs. " + df_matches["Away"]
 matches = df_matches["Confronto"].value_counts().index
 match_selected = st.sidebar.selectbox("Confronto", matches)
-df_match_selected = df_matches[df_matches["Confronto"] == match_selected].iloc[0]
+df_match_selected = df_matches[df_matches["Confronto"].split(" - ")[1] == match_selected].iloc[0]
 
 # Título do dashboard
 st.title("⚽ Análise Completa do Confronto de Futebol")
