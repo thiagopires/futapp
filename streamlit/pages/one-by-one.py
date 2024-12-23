e87b4e560b808300409cbab050d8673258ea88a9
Thiago <thiago.serip@gmail.com>
2024-12-22 22:21:58 -0300

    Update one-by-one.py

--- streamlit/pages/one-by-one.py
+++ streamlit/pages/one-by-one.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import datetime
+from datetime import date
 
 def print_dataframe(df):
     st.dataframe(df, use_container_width=True, hide_index=True)
