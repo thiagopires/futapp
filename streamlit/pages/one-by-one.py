80d29fc17e2f108b9b365baeec7b26f1aec5ed3d
Thiago Pires <thiago.serip@gmail.com>
2025-01-03 15:34:06 -0300

    .

--- streamlit/pages/one-by-one.py
+++ streamlit/pages/one-by-one.py
@@ -326,7 +326,15 @@ with col2:
 
 
 # Tabela 25 e 26: Gols Casa e Visitanted
-df_gols = gols_por_minuto(df_hist, df_match_selected["Home"], df_match_selected["Away"])
+data = {
+    "Home": ["Arsenal", "Ipswich", "Arsenal"],
+    "Away": ["Ipswich", "Arsenal", "Ipswich"],
+    "Goals_H_Minutes": [["10", "20"], ["15", "50"], ["5", "80"]],
+    "Goals_A_Minutes": [["25", "55"], ["30"], ["75"]],
+}
+df = pd.DataFrame(data)
+df_gols = gols_por_minuto(df, "Arsenal", "Ipswich")
+# df_gols = gols_por_minuto(df_hist, df_match_selected["Home"], df_match_selected["Away"])
 st.subheader("Distribuição de Gols por Minuto")
 fig = px.bar(df_gols, x="Range", y=[df_match_selected["Home"], df_match_selected["Away"]], barmode="group", title="Gols por Minuto")
 st.plotly_chart(fig, use_container_width=True)
