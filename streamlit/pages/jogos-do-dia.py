71fd6325a8e0e982257d3614b1edd9f5e0ccbe82
Thiago Pires <thiago.serip@gmail.com>
2025-01-07 00:03:47 -0300

    .

--- streamlit/pages/jogos-do-dia.py
+++ streamlit/pages/jogos-do-dia.py
@@ -43,7 +43,9 @@ def print_dataframe(df, styled_df=None):
         styled_df = styled_df.set_table_styles([
             {'selector': 'th', 'props': [('text-align', 'center')]}
         ])
-    st.dataframe(styled_df, height=len(df)*37, use_container_width=True, hide_index=True)
+        st.dataframe(styled_df, height=len(df)*37, use_container_width=True, hide_index=True)
+    else:
+        st.dataframe(styled_df, use_container_width=True, hide_index=True)
 
 def load_daymatches(i):
     # i = st.session_state.button
