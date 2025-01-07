c6a056b10e661787b56ec2f2818d02361e919d08
Thiago Pires <thiago.serip@gmail.com>
2025-01-06 23:12:06 -0300

    .

--- streamlit/pages/jogos-do-dia.py
+++ streamlit/pages/jogos-do-dia.py
@@ -393,181 +393,180 @@ match_selected = st.dataframe(
     , hide_index=True
 )
 
-if not match_selected.get('selection').get('rows'):
-    exit()
+if match_selected.get('selection').get('rows'):
 
-df_match_selected = df_matches.iloc[match_selected.get('selection').get('rows')[0]]
+    df_match_selected = df_matches.iloc[match_selected.get('selection').get('rows')[0]]
 
-# Título do dashboard
-st.title("⚽ Análise Completa do Confronto de Futebol")
+    # Título do dashboard
+    st.title("⚽ Análise Completa do Confronto de Futebol")
 
-# Header
-st.header(f'{df_match_selected["Confronto"].split("-")[1]}')
-st.subheader(f"{df_match_selected['Formatted_Datetime']} - {df_match_selected["League"]} (Rodada {df_match_selected["Rodada"]})")
-st.divider()
+    # Header
+    st.header(f'{df_match_selected["Confronto"].split("-")[1]}')
+    st.subheader(f"{df_match_selected['Formatted_Datetime']} - {df_match_selected["League"]} (Rodada {df_match_selected["Rodada"]})")
+    st.divider()
 
-# Dividindo a página em duas colunas
-col1, col2 = st.columns(2)
-with col1:
-    st.subheader("Odds")
-    col11, col12, col13 = st.columns(3)
-    col11.metric(label="MO Home", value=df_match_selected["Odd_H_FT"])
-    col12.metric(label="MO Draw", value=df_match_selected["Odd_D_FT"])
-    col13.metric(label="MO Away", value=df_match_selected["Odd_A_FT"])
-    col11.metric(label="Over 0.5 HT", value=df_match_selected["Odd_Over05_HT"])
-    col12.metric(label="Over 2.5 FT", value=df_match_selected["Odd_Over25_FT"])
-    col13.metric(label="BTTS", value=df_match_selected["Odd_BTTS_Yes"])
-with col2:
-    st.subheader("Confrontos diretos nos últimos 3 anos")
-    filter_confrontos = (df_hist["Home"].isin([df_match_selected["Home"], df_match_selected["Away"]])) & (df_hist["Away"].isin([df_match_selected["Home"], df_match_selected["Away"]]))
-    confrontos = df_hist.loc[filter_confrontos, ["Date", "Season", "Home", "Resultado_FT", "Away"]].sort_values(by="Date", ascending=False)
-    df_confrontos = confrontos.style.apply(highlight_result, axis=1, highlight=df_match_selected["Home"])
-    print_dataframe(df_confrontos)
+    # Dividindo a página em duas colunas
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader("Odds")
+        col11, col12, col13 = st.columns(3)
+        col11.metric(label="MO Home", value=df_match_selected["Odd_H_FT"])
+        col12.metric(label="MO Draw", value=df_match_selected["Odd_D_FT"])
+        col13.metric(label="MO Away", value=df_match_selected["Odd_A_FT"])
+        col11.metric(label="Over 0.5 HT", value=df_match_selected["Odd_Over05_HT"])
+        col12.metric(label="Over 2.5 FT", value=df_match_selected["Odd_Over25_FT"])
+        col13.metric(label="BTTS", value=df_match_selected["Odd_BTTS_Yes"])
+    with col2:
+        st.subheader("Confrontos diretos nos últimos 3 anos")
+        filter_confrontos = (df_hist["Home"].isin([df_match_selected["Home"], df_match_selected["Away"]])) & (df_hist["Away"].isin([df_match_selected["Home"], df_match_selected["Away"]]))
+        confrontos = df_hist.loc[filter_confrontos, ["Date", "Season", "Home", "Resultado_FT", "Away"]].sort_values(by="Date", ascending=False)
+        df_confrontos = confrontos.style.apply(highlight_result, axis=1, highlight=df_match_selected["Home"])
+        print_dataframe(df_confrontos)
 
-filter_ultimos_casa = (df_match_selected["Home"] == df_hist["Home"]) | (df_match_selected["Home"] == df_hist["Away"])
-ultimos_casa = df_hist.loc[filter_ultimos_casa, ["Date", "Season", "Home", "Resultado_FT", "Away"]].sort_values(by="Date", ascending=False).head(10)
-df_ultimos_casa = ultimos_casa.style.apply(highlight_result, axis=1, highlight=df_match_selected["Home"])
+    filter_ultimos_casa = (df_match_selected["Home"] == df_hist["Home"]) | (df_match_selected["Home"] == df_hist["Away"])
+    ultimos_casa = df_hist.loc[filter_ultimos_casa, ["Date", "Season", "Home", "Resultado_FT", "Away"]].sort_values(by="Date", ascending=False).head(10)
+    df_ultimos_casa = ultimos_casa.style.apply(highlight_result, axis=1, highlight=df_match_selected["Home"])
 
-filter_ultimos_visitante = (df_match_selected["Away"] == df_hist["Home"]) | (df_match_selected["Away"] == df_hist["Away"])
-ultimos_visitante = df_hist.loc[filter_ultimos_visitante, ["Date", "Season", "Home", "Resultado_FT", "Away"]].sort_values(by="Date", ascending=False).head(10)
-df_ultimos_visitante = ultimos_visitante.style.apply(highlight_result, axis=1, highlight=df_match_selected["Away"])
+    filter_ultimos_visitante = (df_match_selected["Away"] == df_hist["Home"]) | (df_match_selected["Away"] == df_hist["Away"])
+    ultimos_visitante = df_hist.loc[filter_ultimos_visitante, ["Date", "Season", "Home", "Resultado_FT", "Away"]].sort_values(by="Date", ascending=False).head(10)
+    df_ultimos_visitante = ultimos_visitante.style.apply(highlight_result, axis=1, highlight=df_match_selected["Away"])
 
-st.header(f"Últimos 10 Jogos na {df_match_selected['League']}")
+    st.header(f"Últimos 10 Jogos na {df_match_selected['League']}")
 
-col1, col2 = st.columns(2)
-with col1:
-    st.subheader(df_match_selected["Home"])
-    print_dataframe(df_ultimos_casa)
-with col2:
-    st.subheader(df_match_selected["Away"])
-    print_dataframe(df_ultimos_visitante)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader(df_match_selected["Home"])
+        print_dataframe(df_ultimos_casa)
+    with col2:
+        st.subheader(df_match_selected["Away"])
+        print_dataframe(df_ultimos_visitante)
 
-st.subheader("⚽ Classificações nesta competição")
+    st.subheader("⚽ Classificações nesta competição")
 
-filter_classificacao = (df_hist["Season"] == SEASON_ATUAL) & (df_hist["League"] == df_match_selected["League"])
-df_classificacao = df_hist.loc[filter_classificacao, ["League","Season","Date","Rodada","Home","Away","Goals_H_FT","Goals_A_FT"]]
+    filter_classificacao = (df_hist["Season"] == SEASON_ATUAL) & (df_hist["League"] == df_match_selected["League"])
+    df_classificacao = df_hist.loc[filter_classificacao, ["League","Season","Date","Rodada","Home","Away","Goals_H_FT","Goals_A_FT"]]
 
-classificacao_geral = generate_classificacao(df_classificacao, "ALL")
-classificacao_casa = generate_classificacao(df_classificacao, "HOME")
-classificacao_visitante = generate_classificacao(df_classificacao, "AWAY")
+    classificacao_geral = generate_classificacao(df_classificacao, "ALL")
+    classificacao_casa = generate_classificacao(df_classificacao, "HOME")
+    classificacao_visitante = generate_classificacao(df_classificacao, "AWAY")
 
-tab1, tab2, tab3 = st.tabs(["Geral", "Casa", "Visitante"])
-with tab1:
-    # st.subheader("Geral")
-    print_dataframe(classificacao_geral, 740)
-with tab2:
-    # st.subheader("Casa")
-    print_dataframe(classificacao_casa, 740)
-with tab3:
-    # st.subheader("Visitante")
-    print_dataframe(classificacao_visitante, 740)
+    tab1, tab2, tab3 = st.tabs(["Geral", "Casa", "Visitante"])
+    with tab1:
+        # st.subheader("Geral")
+        print_dataframe(classificacao_geral, 740)
+    with tab2:
+        # st.subheader("Casa")
+        print_dataframe(classificacao_casa, 740)
+    with tab3:
+        # st.subheader("Visitante")
+        print_dataframe(classificacao_visitante, 740)
 
-filter_todos_casa = (df_hist["Home"] == df_match_selected["Home"]) & (df_hist["League"] == df_match_selected["League"]) & (df_hist["Season"] == SEASON_ATUAL)
-todos_casa = df_hist.loc[filter_todos_casa, ["Date", "Home", "Resultado_FT", "Away", "Primeiro_Gol"]].sort_values(by="Date", ascending=False)
-df_todos_casa = todos_casa.style.apply(highlight_result, axis=1, highlight=df_match_selected["Home"])
+    filter_todos_casa = (df_hist["Home"] == df_match_selected["Home"]) & (df_hist["League"] == df_match_selected["League"]) & (df_hist["Season"] == SEASON_ATUAL)
+    todos_casa = df_hist.loc[filter_todos_casa, ["Date", "Home", "Resultado_FT", "Away", "Primeiro_Gol"]].sort_values(by="Date", ascending=False)
+    df_todos_casa = todos_casa.style.apply(highlight_result, axis=1, highlight=df_match_selected["Home"])
 
-filter_todos_visitante = (df_hist["Away"] == df_match_selected["Away"]) & (df_hist["League"] == df_match_selected["League"]) & (df_hist["Season"] == SEASON_ATUAL)
-todos_visitante = df_hist.loc[filter_todos_visitante, ["Date", "Home", "Resultado_FT", "Away", "Primeiro_Gol"]].sort_values(by="Date", ascending=False)
-df_todos_visitante = todos_visitante.style.apply(highlight_result, axis=1, highlight=df_match_selected["Away"])
+    filter_todos_visitante = (df_hist["Away"] == df_match_selected["Away"]) & (df_hist["League"] == df_match_selected["League"]) & (df_hist["Season"] == SEASON_ATUAL)
+    todos_visitante = df_hist.loc[filter_todos_visitante, ["Date", "Home", "Resultado_FT", "Away", "Primeiro_Gol"]].sort_values(by="Date", ascending=False)
+    df_todos_visitante = todos_visitante.style.apply(highlight_result, axis=1, highlight=df_match_selected["Away"])
 
-st.header("Todos os jogos Casa/Fora nesta competição")
-st.caption(f'{df_match_selected["League"]} - {SEASON_ATUAL} - Época Normal')
+    st.header("Todos os jogos Casa/Fora nesta competição")
+    st.caption(f'{df_match_selected["League"]} - {SEASON_ATUAL} - Época Normal')
 
-col1, col2 = st.columns(2)
-with col1:
-    st.subheader(df_match_selected["Home"])
-    print_dataframe(df_todos_casa)
-with col2:
-    st.subheader(df_match_selected["Away"])
-    print_dataframe(df_todos_visitante)
-
-
-
-# Tabela 25 e 26: Gols Casa e Visitante
-
-st.subheader("Distribuição de Gols por Minuto")
-
-home_gols_por_tempo = calcular_gols_por_tempo(df_hist.loc[(df_hist["Season"] == SEASON_ATUAL)], df_match_selected["Home"])
-away_gols_por_tempo = calcular_gols_por_tempo(df_hist.loc[(df_hist["Season"] == SEASON_ATUAL)], df_match_selected["Away"])
-
-col1, col2 = st.columns(2)
-with col1:
-    # print_dataframe(home_gols_por_tempo)
-    fig = px.bar(home_gols_por_tempo, 
-                 x=['Gols Marcados','Gols Sofridos'],
-                 y='Intervalo',
-                 orientation="h",
-                 barmode='group',
-                 text_auto=True,
-                 title=df_match_selected['Home']
-    )
-    st.plotly_chart(fig, use_container_width=True, key="fig1")
-with col2:
-    # print_dataframe(away_gols_por_tempo)
-    fig = px.bar(away_gols_por_tempo, 
-                 x=['Gols Marcados','Gols Sofridos'],
-                 y='Intervalo',
-                 orientation="h",
-                 barmode='group',
-                 text_auto=True,
-                 title=df_match_selected['Away']
-    )
-    st.plotly_chart(fig, use_container_width=True, key="fig2")
-
-# col1, col2 = st.columns(2)
-# with col1:
-#     st.plotly_chart(gols_por_minuto(df_hist, df_match_selected["Home"]), use_container_width=True, key="fig1")
-# with col2:
-#     st.plotly_chart(gols_por_minuto(df_hist, df_match_selected["Away"]), use_container_width=True, key="fig2")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader(df_match_selected["Home"])
+        print_dataframe(df_todos_casa)
+    with col2:
+        st.subheader(df_match_selected["Away"])
+        print_dataframe(df_todos_visitante)
 
 
 
-# Tabela 22 e 23: Percurso Casa e Visitante
-# percurso_casa = {
-#     "Sequência de Vitórias": 1,
-#     "Sequência de Empates": 0,
-#     "Sequência de Derrotas": 3,
-#     "Não ganha há": 7,
-#     "Não empata há": 1,
-#     "Não perde há": 3,
-# }
-# percurso_visitante = {
-#     "Sequência de Vitórias": 2,
-#     "Sequência de Empates": 1,
-#     "Sequência de Derrotas": 0,
-#     "Não ganha há": 1,
-#     "Não empata há": 2,
-#     "Não perde há": 5,
-# }
-# col1, col2 = st.columns(2)
-# with col1:
-#     st.subheader("Percurso - Casa")
-#     st.json(percurso_casa)
-# with col2:
-#     st.subheader("Percurso - Visitante")
-#     st.json(percurso_visitante)    
+    # Tabela 25 e 26: Gols Casa e Visitante
 
-# Título
-st.subheader("⚽ Estatísticas de Confrontos de Futebol")
+    st.subheader("Distribuição de Gols por Minuto")
 
-# Dados Simulados para Times
-df_home_estatisticas = calcular_estatisticas(df_hist, df_match_selected['Home'])
-df_home_estatisticas_adicionais = calcular_estatisticas_adicionais(df_hist, df_match_selected['Home'], 'Home')
+    home_gols_por_tempo = calcular_gols_por_tempo(df_hist.loc[(df_hist["Season"] == SEASON_ATUAL)], df_match_selected["Home"])
+    away_gols_por_tempo = calcular_gols_por_tempo(df_hist.loc[(df_hist["Season"] == SEASON_ATUAL)], df_match_selected["Away"])
 
-df_away_estatisticas = calcular_estatisticas(df_hist, df_match_selected['Away'])
-df_away_estatisticas_adicionais = calcular_estatisticas_adicionais(df_hist, df_match_selected['Away'], 'Away')
+    col1, col2 = st.columns(2)
+    with col1:
+        # print_dataframe(home_gols_por_tempo)
+        fig = px.bar(home_gols_por_tempo, 
+                    x=['Gols Marcados','Gols Sofridos'],
+                    y='Intervalo',
+                    orientation="h",
+                    barmode='group',
+                    text_auto=True,
+                    title=df_match_selected['Home']
+        )
+        st.plotly_chart(fig, use_container_width=True, key="fig1")
+    with col2:
+        # print_dataframe(away_gols_por_tempo)
+        fig = px.bar(away_gols_por_tempo, 
+                    x=['Gols Marcados','Gols Sofridos'],
+                    y='Intervalo',
+                    orientation="h",
+                    barmode='group',
+                    text_auto=True,
+                    title=df_match_selected['Away']
+        )
+        st.plotly_chart(fig, use_container_width=True, key="fig2")
 
-col1, col2 = st.columns(2)
-with col1:
-    st.subheader(df_match_selected["Home"])
-    print_dataframe(df_home_estatisticas.to_dict(orient='records'))
-    print_dataframe(df_home_estatisticas_adicionais.to_dict(orient='records'))
-with col2:
-    st.subheader(df_match_selected["Away"])
-    print_dataframe(df_away_estatisticas.to_dict(orient='records'))
-    print_dataframe(df_away_estatisticas_adicionais.to_dict(orient='records'))
+    # col1, col2 = st.columns(2)
+    # with col1:
+    #     st.plotly_chart(gols_por_minuto(df_hist, df_match_selected["Home"]), use_container_width=True, key="fig1")
+    # with col2:
+    #     st.plotly_chart(gols_por_minuto(df_hist, df_match_selected["Away"]), use_container_width=True, key="fig2")
 
-# Outros dados e análises podem ser adicionados conforme necessário
-st.write("⚡ Dashboard dinâmico para análise de confrontos! ⚡")
+
+
+    # Tabela 22 e 23: Percurso Casa e Visitante
+    # percurso_casa = {
+    #     "Sequência de Vitórias": 1,
+    #     "Sequência de Empates": 0,
+    #     "Sequência de Derrotas": 3,
+    #     "Não ganha há": 7,
+    #     "Não empata há": 1,
+    #     "Não perde há": 3,
+    # }
+    # percurso_visitante = {
+    #     "Sequência de Vitórias": 2,
+    #     "Sequência de Empates": 1,
+    #     "Sequência de Derrotas": 0,
+    #     "Não ganha há": 1,
+    #     "Não empata há": 2,
+    #     "Não perde há": 5,
+    # }
+    # col1, col2 = st.columns(2)
+    # with col1:
+    #     st.subheader("Percurso - Casa")
+    #     st.json(percurso_casa)
+    # with col2:
+    #     st.subheader("Percurso - Visitante")
+    #     st.json(percurso_visitante)    
+
+    # Título
+    st.subheader("⚽ Estatísticas de Confrontos de Futebol")
+
+    # Dados Simulados para Times
+    df_home_estatisticas = calcular_estatisticas(df_hist, df_match_selected['Home'])
+    df_home_estatisticas_adicionais = calcular_estatisticas_adicionais(df_hist, df_match_selected['Home'], 'Home')
+
+    df_away_estatisticas = calcular_estatisticas(df_hist, df_match_selected['Away'])
+    df_away_estatisticas_adicionais = calcular_estatisticas_adicionais(df_hist, df_match_selected['Away'], 'Away')
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader(df_match_selected["Home"])
+        print_dataframe(df_home_estatisticas.to_dict(orient='records'))
+        print_dataframe(df_home_estatisticas_adicionais.to_dict(orient='records'))
+    with col2:
+        st.subheader(df_match_selected["Away"])
+        print_dataframe(df_away_estatisticas.to_dict(orient='records'))
+        print_dataframe(df_away_estatisticas_adicionais.to_dict(orient='records'))
+
+    # Outros dados e análises podem ser adicionados conforme necessário
+    st.write("⚡ Dashboard dinâmico para análise de confrontos! ⚡")
 
 
