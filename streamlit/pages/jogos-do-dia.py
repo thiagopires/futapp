4bdeb65d5b1c39be3307d8fb4efbb3377f0882c4
Thiago Pires <thiago.serip@gmail.com>
2025-01-07 21:08:44 -0300

    .

--- streamlit/pages/jogos-do-dia.py
+++ streamlit/pages/jogos-do-dia.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import ast
-from datetime import datetime, timedelta
+from datetime import datetime
 
 SEASON_ATUAL = '2024/2025'
 
