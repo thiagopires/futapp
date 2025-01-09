127f57ef14bd0a944c42ccef0206b9c192e790ca
Thiago Pires <thiago.serip@gmail.com>
2025-01-09 15:52:18 -0300

    .

--- streamlit/pages/calculadora.py
+++ streamlit/pages/calculadora.py
@@ -5,48 +5,50 @@ from utils.functions import *
 st.set_page_config(layout="wide")
 st.title("⚽ Calculadora")
 
-col_cashout, col_freebet = st.columns(2)
+col_cashout, col_resultado = st.columns(2)
+
+# Coluna de Cashout
 with col_cashout:
     st.header("Cashout")
-    tab_bl, tab_lb = st.tabs(["Back/Lay","Lay/Back"])
+    tab_bl, tab_lb = st.tabs(["Back/Lay", "Lay/Back"])
 
+    # Aba Back/Lay
     with tab_bl:
-        odd_back = st.number_input("Odd Back", key="bl_odd_back")
-        stake_back = st.number_input("Stake Back", key="bl_stake_back")
-        odd_lay = st.number_input("Odd Lay", key="bl_odd_lay")       
+        st.session_state['bl_odd_back'] = st.number_input("Odd Back", key="bl_odd_back")
+        st.session_state['bl_stake_back'] = st.number_input("Stake Back", key="bl_stake_back")
+        st.session_state['bl_odd_lay'] = st.number_input("Odd Lay", key="bl_odd_lay")
 
+    # Aba Lay/Back
     with tab_lb:
-        odd_lay = st.number_input("Odd Lay",key="lb_odd_lay")
-        responsabilidade_lay = st.number_input("Responsabilidade Lay", key="lb_responsabilidade_lay")
-        odd_back = st.number_input("Odd Back", key="lb_odd_back")
+        st.session_state['lb_odd_lay'] = st.number_input("Odd Lay", key="lb_odd_lay")
+        st.session_state['lb_responsabilidade_lay'] = st.number_input("Responsabilidade Lay", key="lb_responsabilidade_lay")
+        st.session_state['lb_odd_back'] = st.number_input("Odd Back", key="lb_odd_back")
 
-with col_freebet:
-    st.write("ok")
+with col_resultado:
 
-    if odd_back and stake_back and odd_lay:
-        stake_lay = stake_back/odd_lay*odd_back
-        lucro_perda = str(round(stake_lay-stake_back, 2))
+    # Back/Lay
+    if st.session_state.get('bl_odd_back') and st.session_state.get('bl_stake_back') and st.session_state.get('bl_odd_lay'):
+        stake_lay = st.session_state['bl_stake_back'] / st.session_state['bl_odd_lay'] * st.session_state['bl_odd_back']
+        lucro_perda = round(stake_lay - st.session_state['bl_stake_back'], 2)
 
-        st.write(f"Stake de Lay:")           
-        container1 = st.container(border=True)
-        container1.code(str(round(stake_lay, 2)), language="text")
-        
+        st.write(f"Stake de Lay:")
+        container = st.container(border=True)
+        container.code(str(round(stake_lay, 2)), language="text")
+
         st.write(f"Perda/Lucro:")
-        container2 = st.container(border=True)
-        container2.code(str(lucro_perda), language="text")
+        container = st.container(border=True)
+        container.code(str(lucro_perda), language="text")
 
-    if odd_lay and responsabilidade_lay and odd_back:
-        stake_lay = responsabilidade_lay / (odd_lay - 1)
-        stake_back = stake_lay * odd_lay / odd_back
-        lucro_perda = round((stake_back * (odd_back - 1)) - responsabilidade_lay, 2)
-        
+    # Lay/Back
+    if st.session_state.get('lb_odd_lay') and st.session_state.get('lb_responsabilidade_lay') and st.session_state.get('lb_odd_back'):
+        stake_lay = st.session_state['lb_responsabilidade_lay'] / (st.session_state['lb_odd_lay'] - 1)
+        stake_back = stake_lay * st.session_state['lb_odd_lay'] / st.session_state['lb_odd_back']
+        lucro_perda = round((stake_back * (st.session_state['lb_odd_back'] - 1)) - st.session_state['lb_responsabilidade_lay'], 2)
+
         st.write(f"Stake de Back:")
-        container3 = st.container(border=True)
-        container3.code(str(round(stake_back, 2)), language="text")
+        container = st.container(border=True)
+        container.code(str(round(stake_back, 2)), language="text")
 
         st.write(f"Perda/Lucro:")
-        container4 = st.container(border=True)
-        container4.code(str(lucro_perda), language="text") 
-
-    st.write(odd_back)   
-    st.write(odd_lay)    
+        container = st.container(border=True)
+        container.code(str(lucro_perda), language="text")
