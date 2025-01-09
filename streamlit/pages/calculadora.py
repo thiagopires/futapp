e58321972210e69509116c8cd63f0bc8be5ad3e5
Thiago Pires <thiago.serip@gmail.com>
2025-01-09 17:45:12 -0300

    .

--- streamlit/pages/calculadora.py
+++ streamlit/pages/calculadora.py
@@ -33,30 +33,31 @@ with col_cashout:
         st.number_input("Odd Back", key="lb_odd_back")
 
 with col_resultado:
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        # Back/Lay
+        if aba == "Back/Lay" and st.session_state.get('bl_odd_back') and st.session_state.get('bl_stake_back') and st.session_state.get('bl_odd_lay'):
+            stake_lay = st.session_state['bl_stake_back'] / st.session_state['bl_odd_lay'] * st.session_state['bl_odd_back']
+            lucro_perda = round(stake_lay - st.session_state['bl_stake_back'], 2)
 
-    # Back/Lay
-    if aba == "Back/Lay" and st.session_state.get('bl_odd_back') and st.session_state.get('bl_stake_back') and st.session_state.get('bl_odd_lay'):
-        stake_lay = st.session_state['bl_stake_back'] / st.session_state['bl_odd_lay'] * st.session_state['bl_odd_back']
-        lucro_perda = round(stake_lay - st.session_state['bl_stake_back'], 2)
+            st.write(f"Stake de Lay:")
+            container = st.container(border=True)
+            container.code(str(round(stake_lay, 2)), language="text")
 
-        st.write(f"Stake de Lay:")
-        container = st.container(border=True)
-        container.code(str(round(stake_lay, 2)), language="text")
+            st.write(f"Perda/Lucro:")
+            container = st.container(border=True)
+            container.code(str(lucro_perda), language="text")
 
-        st.write(f"Perda/Lucro:")
-        container = st.container(border=True)
-        container.code(str(lucro_perda), language="text")
+        # Lay/Back
+        if aba == "Lay/Back" and st.session_state.get('lb_odd_lay') and st.session_state.get('lb_responsabilidade_lay') and st.session_state.get('lb_odd_back'):
+            stake_lay = st.session_state['lb_responsabilidade_lay'] / (st.session_state['lb_odd_lay'] - 1)
+            stake_back = stake_lay * st.session_state['lb_odd_lay'] / st.session_state['lb_odd_back']
+            lucro_perda = round((stake_back * (st.session_state['lb_odd_back'] - 1)) - st.session_state['lb_responsabilidade_lay'], 2)
 
-    # Lay/Back
-    if aba == "Lay/Back" and st.session_state.get('lb_odd_lay') and st.session_state.get('lb_responsabilidade_lay') and st.session_state.get('lb_odd_back'):
-        stake_lay = st.session_state['lb_responsabilidade_lay'] / (st.session_state['lb_odd_lay'] - 1)
-        stake_back = stake_lay * st.session_state['lb_odd_lay'] / st.session_state['lb_odd_back']
-        lucro_perda = round((stake_back * (st.session_state['lb_odd_back'] - 1)) - st.session_state['lb_responsabilidade_lay'], 2)
+            st.write(f"Stake de Back:")
+            container = st.container(border=True)
+            container.code(str(round(stake_back, 2)), language="text")
 
-        st.write(f"Stake de Back:")
-        container = st.container(border=True)
-        container.code(str(round(stake_back, 2)), language="text")
-
-        st.write(f"Perda/Lucro:")
-        container = st.container(border=True)
-        container.code(str(lucro_perda), language="text")
+            st.write(f"Perda/Lucro:")
+            container = st.container(border=True)
+            container.code(str(lucro_perda), language="text")
