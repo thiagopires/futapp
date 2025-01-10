04db7ad2b5b048a3c5e47a69c7474e3566f73659
Thiago Pires <thiago.serip@gmail.com>
2025-01-10 12:39:02 -0300

    .

--- streamlit/pages/analise-home.py
+++ streamlit/pages/analise-home.py
@@ -125,7 +125,7 @@ if st.session_state['active_button'] == "Over 2.5 FT / BTTS":
     df_hist_mandante_over25 = df_hist.loc[(df_hist['Home'] == mandante)]
     over25['Jogos analisados'] = len(df_hist_mandante_over25)
 
-    df_hist_mandante_over25 = df_hist_mandante_over25.loc[(df_hist['Total_Goals_FT'] == mandante)]
+    df_hist_mandante_over25 = df_hist_mandante_over25.loc[(df_hist['TotalGoals_FT'] > 2.5)]
     over25['Jogos Over 2.5 FT'] = len(df_hist_mandante_over25)
     
     over25['Winrate'] = f"{str(round(over25['Jogos Over 2.5 FT'] / over25['Jogos Analisados'] * 100,2))}%"
