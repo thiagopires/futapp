ae0da3f2588341840046b5e5267069e91fed0aea
Thiago Pires <thiago.serip@gmail.com>
2025-01-11 15:40:28 -0300

    .

--- streamlit/pages/analise-home.py
+++ streamlit/pages/analise-home.py
@@ -160,7 +160,7 @@ with colb2:
 
 df_match_selected = df_matches.loc[(df_matches['Home'] == mandante)]
 
-st.dataframe(df_match_selected)
+st.text(df_match_selected['Away'])
 
 # df_hist_mandante_placar = df_hist.loc[
 #     (df_hist['Home'] == mandante) & 
