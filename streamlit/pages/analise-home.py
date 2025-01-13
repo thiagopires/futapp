de3c87d07d9eece94bac4792726e6ede762d1b1c
Thiago Pires <thiago.serip@gmail.com>
2025-01-13 10:19:42 -0300

    .

--- streamlit/pages/analise-home.py
+++ streamlit/pages/analise-home.py
@@ -192,97 +192,98 @@ else:
     st.divider()
 
     st.subheader("Selecione o Mandante e o Placar para a análise")
+    
+    if len(df_matches) > 0:
+        colb1, colb2 = st.columns(2)
+        with colb1:
+            mandante = st.selectbox("Escolha o Mandante", df_matches['Home'])
+        with colb2:
+            placar = st.selectbox("Escolha o Placar", ['0x0','0x1','0x2','0x3','1x0','1x1','1x2','1x3','2x0','2x1','2x2','2x3','3x0','3x1','3x2','3x3'])
 
-    colb1, colb2 = st.columns(2)
-    with colb1:
-        mandante = st.selectbox("Escolha o Mandante", df_matches['Home'])
-    with colb2:
-        placar = st.selectbox("Escolha o Placar", ['0x0','0x1','0x2','0x3','1x0','1x1','1x2','1x3','2x0','2x1','2x2','2x3','3x0','3x1','3x2','3x3'])
-
-    df_match_selected = df_matches.loc[(df_matches['Home'] == mandante)]
+        df_match_selected = df_matches.loc[(df_matches['Home'] == mandante)]
 
 
-    # df_hist_mandante_placar = df_hist.loc[
-    #     (df_hist['Home'] == mandante) & 
-    #     (df_hist['Resultado_FT'].str.replace("-","x") == placar) &
-    #     (df_hist['Season'] == get_current_season()) # vrificar
-    # ]
-
-    # if len(df_hist_mandante_placar) > 0:
-    #     print_dataframe(df_hist_mandante_placar[['League','Rodada','Time','Home','Away','Odd_H_FT','Odd_D_FT','Odd_A_FT','Odd_Over05_HT','Odd_Over15_FT','Odd_Over25_FT','Odd_BTTS_Yes']])
-    # else:
-    #     st.write("Sem jogos com este placar.")
-
-    st.divider()
-
-    st.session_state['active_button'] = ""
-
-    col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
-    with col1:
-        if st.button("Profit Acumulado", use_container_width=True):
-            st.session_state['active_button'] = "Profit Acumulado"
-    with col2:
-        st.button("Ponto de Saída Punter", use_container_width=True)
-        st.button("Ocorrências Gerais", use_container_width=True)
-    with col3:
-        st.button("Ponto de Saída Trader", use_container_width=True)
-        st.button("Ponto de Revisão", use_container_width=True)
-    with col4:
-        if st.button("Últimos 10 jogos", use_container_width=True):
-            st.session_state['active_button'] = "Últimos 10 jogos"
-        if st.button("Confronto Direto", use_container_width=True):
-            st.session_state['active_button'] = "Confronto Direto"
-    with col5:
-        st.button("Temporada Atual", use_container_width=True)
-        st.button("Temporada Anterior", use_container_width=True)
-    with col6:
-        if st.button("Match Odds - Back", use_container_width=True):
-            st.session_state['active_button'] = "Match Odds - Back"
-        if st.button("Match Odds - Lay", use_container_width=True):
-            st.session_state['active_button'] = "Match Odds - Lay"
-    with col7:
-        if st.button("Over 2.5 FT / BTTS", use_container_width=True):
-            st.session_state['active_button'] = "Over 2.5 FT / BTTS"
-            
-    ###
-
-    if len(df_match_selected) > 0:
-
-        visitante = df_match_selected.iloc[0]["Away"]
-
-        if st.session_state['active_button'] == "Over 2.5 FT / BTTS":
-                    
-            st.subheader(f"Over 2.5 FT nos jogos do {mandante}")
-            st.write(f"Jogos anteriores do {mandante} que bateram o Over 2.5 FT")    
-            aba_over25(df_hist, mandante, "Home")
-
-            st.subheader(f"BTTS nos jogos do {mandante}")
-            st.write(f"Jogos anteriores do {mandante} que bateram o BTTS")
-            aba_btts(df_hist, mandante, "Home")
-
-        elif st.session_state['active_button'] == "Últimos 10 jogos":
-
-            st.subheader(f"Últimos 10 jogos do {mandante} como Mandante")
-            aba_ult10(df_hist, mandante, "Home")
-
-            st.subheader(f"Últimos 10 jogos do {visitante} como Visitante")
-            aba_ult10(df_hist, visitante, "Away")
-
-        elif st.session_state['active_button'] == "Confronto Direto":
-            st.subheader(f"Confronto direto - Temporadas passadas")
-            aba_confrontodireto(df_hist, mandante, visitante)
-
-        elif st.session_state['active_button'] == "Match Odds - Back":
-            st.subheader(f"Back Home (Apostar no {mandante})")
-            aba_back_home(df_hist, mandante)
-
-        # df_hist_mandante_btts = df_hist.loc[
+        # df_hist_mandante_placar = df_hist.loc[
         #     (df_hist['Home'] == mandante) & 
-        #     (df_hist['Goals_H_FT'] >= 1) &
-        #     (df_hist['Goals_A_FT'] >= 1)
+        #     (df_hist['Resultado_FT'].str.replace("-","x") == placar) &
+        #     (df_hist['Season'] == get_current_season()) # vrificar
         # ]
 
-        # if len(df_hist_mandante_btts) > 0:
-        #     print_dataframe(df_hist_mandante_btts[['League','Rodada','Time','Home','Away','Odd_BTTS_Yes','Goals_H_FT','Goals_A_FT']])
+        # if len(df_hist_mandante_placar) > 0:
+        #     print_dataframe(df_hist_mandante_placar[['League','Rodada','Time','Home','Away','Odd_H_FT','Odd_D_FT','Odd_A_FT','Odd_Over05_HT','Odd_Over15_FT','Odd_Over25_FT','Odd_BTTS_Yes']])
         # else:
-        #     st.write("Sem jogos.")
+        #     st.write("Sem jogos com este placar.")
+
+        st.divider()
+
+        st.session_state['active_button'] = ""
+
+        col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
+        with col1:
+            if st.button("Profit Acumulado", use_container_width=True):
+                st.session_state['active_button'] = "Profit Acumulado"
+        with col2:
+            st.button("Ponto de Saída Punter", use_container_width=True)
+            st.button("Ocorrências Gerais", use_container_width=True)
+        with col3:
+            st.button("Ponto de Saída Trader", use_container_width=True)
+            st.button("Ponto de Revisão", use_container_width=True)
+        with col4:
+            if st.button("Últimos 10 jogos", use_container_width=True):
+                st.session_state['active_button'] = "Últimos 10 jogos"
+            if st.button("Confronto Direto", use_container_width=True):
+                st.session_state['active_button'] = "Confronto Direto"
+        with col5:
+            st.button("Temporada Atual", use_container_width=True)
+            st.button("Temporada Anterior", use_container_width=True)
+        with col6:
+            if st.button("Match Odds - Back", use_container_width=True):
+                st.session_state['active_button'] = "Match Odds - Back"
+            if st.button("Match Odds - Lay", use_container_width=True):
+                st.session_state['active_button'] = "Match Odds - Lay"
+        with col7:
+            if st.button("Over 2.5 FT / BTTS", use_container_width=True):
+                st.session_state['active_button'] = "Over 2.5 FT / BTTS"
+                
+        ###
+
+        if len(df_match_selected) > 0:
+
+            visitante = df_match_selected.iloc[0]["Away"]
+
+            if st.session_state['active_button'] == "Over 2.5 FT / BTTS":
+                        
+                st.subheader(f"Over 2.5 FT nos jogos do {mandante}")
+                st.write(f"Jogos anteriores do {mandante} que bateram o Over 2.5 FT")    
+                aba_over25(df_hist, mandante, "Home")
+
+                st.subheader(f"BTTS nos jogos do {mandante}")
+                st.write(f"Jogos anteriores do {mandante} que bateram o BTTS")
+                aba_btts(df_hist, mandante, "Home")
+
+            elif st.session_state['active_button'] == "Últimos 10 jogos":
+
+                st.subheader(f"Últimos 10 jogos do {mandante} como Mandante")
+                aba_ult10(df_hist, mandante, "Home")
+
+                st.subheader(f"Últimos 10 jogos do {visitante} como Visitante")
+                aba_ult10(df_hist, visitante, "Away")
+
+            elif st.session_state['active_button'] == "Confronto Direto":
+                st.subheader(f"Confronto direto - Temporadas passadas")
+                aba_confrontodireto(df_hist, mandante, visitante)
+
+            elif st.session_state['active_button'] == "Match Odds - Back":
+                st.subheader(f"Back Home (Apostar no {mandante})")
+                aba_back_home(df_hist, mandante)
+
+            # df_hist_mandante_btts = df_hist.loc[
+            #     (df_hist['Home'] == mandante) & 
+            #     (df_hist['Goals_H_FT'] >= 1) &
+            #     (df_hist['Goals_A_FT'] >= 1)
+            # ]
+
+            # if len(df_hist_mandante_btts) > 0:
+            #     print_dataframe(df_hist_mandante_btts[['League','Rodada','Time','Home','Away','Odd_BTTS_Yes','Goals_H_FT','Goals_A_FT']])
+            # else:
+            #     st.write("Sem jogos.")
