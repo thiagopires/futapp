6eb57f1d922af0696914c696f70bdfd5585372b9
Thiago Pires <thiago.serip@gmail.com>
2025-01-17 14:10:24 -0300

    .

--- streamlit/pages/backtesting.py
+++ streamlit/pages/backtesting.py
@@ -130,7 +130,7 @@ else:
 
         # Agrupar por data e somar os lucros
         daily_profit = df_hist.groupby("Date")["Profit"].sum().reset_index()
-        daily_profit = daily_profit.sort_values(by="Date", ascending=False)
+        # daily_profit = daily_profit.sort_values(by="Date", ascending=False)
         daily_profit["Cumulative_Profit"] = daily_profit["Profit"].cumsum()
         st.dataframe(daily_profit)
 
