3ee373f4b1ac820bbf0db00273a1dcf3cfb002ee
Thiago Pires <thiago.serip@gmail.com>
2025-01-18 12:20:55 -0300

    .

--- streamlit/pages/jogos-do-dia.py
+++ streamlit/pages/jogos-do-dia.py
@@ -33,7 +33,7 @@ else:
     # Dataframe
     st.subheader(f"Selecione o jogo:")
     match_selected = st.dataframe(
-        df_matches[['League','Rodada','Time','Home','Away','Odd_H_FT','Odd_D_FT','Odd_A_FT','Odd_Over05_HT','Odd_Over15_FT','Odd_Over25_FT','Odd_BTTS_Yes']]
+        df_matches[['League','Rodada','Time','Home','Away','Odd_H_FT','Odd_D_FT','Odd_A_FT','Odd_Over05_HT','Odd_Over15_FT','Odd_Over25_FT','Odd_BTTS_Yes','XG_Total_Pre','XG_Home_Pre','XG_Away_Pre']]
         , on_select="rerun"
         , selection_mode="single-row"
         , use_container_width=True
