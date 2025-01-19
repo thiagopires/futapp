80b799473246b15b84dcd0f88ac47efedada8dc5
Thiago Pires <thiago.serip@gmail.com>
2025-01-19 16:20:16 -0300

    .

--- streamlit/utils/functions.py
+++ streamlit/utils/functions.py
@@ -38,7 +38,7 @@ def get_filter_lay_zebra(df): # 44257907
             'Italy Serie B',
             'Portugal Liga NOS',
             'Spain La Liga',
-            'Spain Segunda División',
+            # 'Spain Segunda División',
             'Turkey Süper Lig'
         ]))
     )
