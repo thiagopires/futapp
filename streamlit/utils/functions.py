b4f9c36608535ddecd1082c561e9be1154f319ae
Thiago Pires <thiago.serip@gmail.com>
2025-01-20 00:40:07 -0300

    .

--- streamlit/utils/functions.py
+++ streamlit/utils/functions.py
@@ -23,6 +23,7 @@ def get_filter_lay_zebra(df): # 44257907
         # (df["Odd_H_FT"] >= 1.5) & 
         ((df["Odd_H_FT"] <= 1.8) | (df["Odd_H_FT"] >= 1.9)) &
         # (df["Odd_A_FT"] < 5) &
+        (df["Odd_BTTS_Yes"] >= 1.8) &
         (df["Odd_BTTS_Yes"] < 2) &
         (df["Odd_Over25_FT"] > 1.5) &
         (df["XG_Home_Pre"] > df["XG_Away_Pre"]) &
