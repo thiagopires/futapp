4cff39b7811152c1706d4673e17deb57206619f2
Thiago Pires <thiago.serip@gmail.com>
2025-01-19 21:42:54 -0300

    .

--- streamlit/utils/functions.py
+++ streamlit/utils/functions.py
@@ -20,23 +20,23 @@ def get_filter_lay_zebra(df): # 44257907
     return (
         (df["Odd_H_FT"] < df["Odd_D_FT"]) &
         (df["Odd_D_FT"] < df["Odd_A_FT"]) &
-        (df["Odd_H_FT"] >= 1.5) &
+        (df["Odd_H_FT"] >= 1.55) &
         (df["Odd_BTTS_Yes"] < 2) &
         (df["XG_Total_Pre"] >= 1.7) &
         (df["XG_Away_Pre"] <= 1.25) &
         (df["XG_Home_Pre"] > df["XG_Away_Pre"]) &
         (df["Rodada"] > 2) &
-        # ((df["Odd_H_FT"] <= 1.8) | (df["Odd_H_FT"] >= 2)) &
+        ((df["Odd_H_FT"] <= 1.8) | (df["Odd_H_FT"] >= 1.9)) &
         (df['League'].isin([
             'Belgium Pro League',
-            'England EFL League One',
+            # 'England EFL League One',
             'England Premier League',
             'France Ligue 1',
-                # 'France Ligue 2',
+                #'France Ligue 2',
             'Germany 2. Bundesliga',
             'Germany Bundesliga',
             'Italy Serie A',
-            'Italy Serie B',
+                # 'Italy Serie B',
                 # 'Netherlands Eredivisie',
                 # 'Netherlands Eerste Divisie',
             'Portugal Liga NOS',
