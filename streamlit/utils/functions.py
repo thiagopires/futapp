3475026a9af79a11341b2d56e0931d0e5494413c
Thiago Pires <thiago.serip@gmail.com>
2025-01-19 23:53:36 -0300

    .

--- streamlit/utils/functions.py
+++ streamlit/utils/functions.py
@@ -36,14 +36,14 @@ def get_filter_lay_zebra(df): # 44257907
             'England Premier League',
             # 'England Championship',
             'France Ligue 1',
-            # 'France Ligue 2',
+            'France Ligue 2',
             'Germany 2. Bundesliga',
             'Germany Bundesliga',
             'Italy Serie A',
             # 'Italy Serie B',
             'Portugal Liga NOS',
             'Spain La Liga',
-            # 'Spain Segunda División',
+            'Spain Segunda División',
             'Turkey Süper Lig'
         ]))
     )
