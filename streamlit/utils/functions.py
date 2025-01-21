ed22a8321663163aa559175e8e7cc3c964dadd37
Thiago Pires <thiago.serip@gmail.com>
2025-01-20 23:14:54 -0300

    .

--- streamlit/utils/functions.py
+++ streamlit/utils/functions.py
@@ -22,8 +22,8 @@ def get_filter_over(df):
         (df["XG_Away_Pre"] >= 1.3) &
         (
             (
-            (df["Odd_H_FT"] < df["Odd_D_FT"]) &
-            (df["Odd_D_FT"] < df["Odd_A_FT"]) &
+            (df["Odd_H_FT"] <= df["Odd_A_FT"]) &
+            # (df["Odd_D_FT"] < df["Odd_A_FT"]) &
             (df['League'].isin([
                 'Spain La Liga',
                 'France Ligue 1',
@@ -32,8 +32,8 @@ def get_filter_over(df):
                 'England Premier League'
             ]))
             ) | (
-            (df["Odd_H_FT"] > df["Odd_D_FT"]) &
-            (df["Odd_D_FT"] > df["Odd_A_FT"]) &
+            (df["Odd_H_FT"] >= df["Odd_A_FT"]) &
+            # (df["Odd_D_FT"] > df["Odd_A_FT"]) &
             (df['League'].isin([
                 'England EFL League One',
                 'Italy Serie B',
