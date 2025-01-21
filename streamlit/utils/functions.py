c5edf0f4a36df970f5bb75c4aae603fe02813ff3
Thiago Pires <thiago.serip@gmail.com>
2025-01-20 23:58:09 -0300

    .

--- streamlit/utils/functions.py
+++ streamlit/utils/functions.py
@@ -19,11 +19,11 @@ def get_today(offset=0):
 def get_filter_over(df):
     return (
         (df["XG_Home_Pre"] >= 1.3) &
-        (df["XG_Away_Pre"] >= 1.3) &
-        # (abs(df["Odd_H_FT"] - df["Odd_A_FT"]) > 0.2) &
+        (df["XG_Away_Pre"] >= 1.3) &        
         (
             (
                 (df["Odd_H_FT"] <= df["Odd_A_FT"]) &
+                (abs(df["Odd_H_FT"] - df["Odd_A_FT"]) > 0.2) &
                 (df['League'].isin([
                     'Spain La Liga',
                     'Portugal Liga NOS',
