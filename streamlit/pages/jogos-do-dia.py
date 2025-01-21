8a02c34c3787f5b584c2e2fc6f58cfc7a6a4a2c3
Thiago Pires <thiago.serip@gmail.com>
2025-01-20 22:12:20 -0300

    .

--- streamlit/pages/jogos-do-dia.py
+++ streamlit/pages/jogos-do-dia.py
@@ -31,8 +31,8 @@ else:
 
     filtro_over25_ft = st.checkbox("Over 2.5 FT")
     if filtro_over25_ft:
-        filter = get_filter_over25_ft(df_hist)
-        df_hist = df_hist[filter] 
+        filter = get_filter_over25_ft(df_matches)
+        df_matches = df_matches[filter] 
         metodo = 'Over 2.5 FT'
 
     # Dataframe
