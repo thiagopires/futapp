e280c65bdad29a26e15180eeaf1bf0e565a8d3fc
Thiago Pires <thiago.serip@gmail.com>
2025-01-21 15:41:49 -0300

    .

--- streamlit/utils/functions.py
+++ streamlit/utils/functions.py
@@ -151,7 +151,7 @@ def get_filter_lay_visitante_zebra(df):
         ((df["Odd_H_FT"] <= 1.8) | (df["Odd_H_FT"] > 1.9)) &
         (df["Odd_A_FT"] < 10) &
         (df["Odd_BTTS_Yes"] < 2) &
-        (df["Odd_Over25_FT"] > 1.5) &
+        (df["Odd_Over25_FT"] > 1.6) &
         (df["XG_Home_Pre"] > df["XG_Away_Pre"]) &
         (df["XG_Total_Pre"] >= 1.7) &
         (df["XG_Away_Pre"] <= 1.25) &
