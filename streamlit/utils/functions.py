c271acad1386007c2c8961be4c010c23582f0cd0
Thiago Pires <thiago.serip@gmail.com>
2025-01-22 00:38:59 -0300

    .

--- streamlit/utils/functions.py
+++ streamlit/utils/functions.py
@@ -217,16 +217,23 @@ def get_filter_back_empate(df):
         (df["XG_Home_Pre"] < 1.6) &
         (df["XG_Away_Pre"] < 1.6) &
         (df["Odd_BTTS_Yes"] < 2) &
-        (df["Odd_Over25_FT"] > 1.5) # &
-        # (df['League'].isin([
-        #     "Portugal Liga NOS",
-        #     "Turkey Süper Lig",
-        #     "Belgium Pro League",
-        #     "Romania Liga I",
-        #     "Italy Serie B",
-        #     "England Championship",
-        #     "England Premier League"
-        # ]))
+        (df["Odd_Over25_FT"] > 1.5) &
+        (df['League'].isin([
+            # "Portugal Liga NOS",
+            # "Turkey Süper Lig",
+            # "Belgium Pro League",
+            # "Romania Liga I",
+            # "Italy Serie B",
+            # "England Championship",
+            # "England Premier League"
+
+            "England Championship",
+            "Belgium Pro League",
+            "Romania Liga I",
+            "Italy Serie B",
+            "Turkey Süper Lig",
+            "Portugal Liga NOS"
+        ]))
     )
 
 def print_dataframe(df, styled_df=None):
