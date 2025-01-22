a0c49adaecaf2decf931af1679e33480fdbdcdaf
Thiago Pires <thiago.serip@gmail.com>
2025-01-22 09:47:23 -0300

    .

--- streamlit/utils/functions.py
+++ streamlit/utils/functions.py
@@ -213,8 +213,7 @@ def get_filter_lay_visitante_zebra(df):
 
 def get_filter_back_empate(df):
     return (
-        (df["Odd_H_FT"] < 6) & 
-        (df["Odd_H_FT"] > 1.5) & # | ((df["Odd_A_FT"] < 6) & (df["Odd_A_FT"] > 1.5))) &
+        (df["Odd_H_FT"] > 1.5) & (df["Odd_H_FT"] < 6) &         
         ((df["Odd_H_FT"] < 1.8) | (df["Odd_H_FT"] > 1.9)) &        
         (df["XG_Home_Pre"] > 0) & (df["XG_Home_Pre"] < 1.8) &
         (df["XG_Away_Pre"] > 0) & (df["XG_Away_Pre"] < 1.8) &
