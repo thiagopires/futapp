e2e62e1ba02111e1eb04123be8380c45dd3b7e3b
Thiago Pires <thiago.serip@gmail.com>
2025-01-21 22:15:20 -0300

    .

--- streamlit/app.py
+++ streamlit/app.py
@@ -10,7 +10,7 @@ def login_page():
         #MainMenu {visibility: hidden;}
         footer {visibility: hidden;}
         </style>
-        """
+    """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
     st.title("Login")
