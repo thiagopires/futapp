56afdf6a3a6dca9186e49860115540c7fae18aef
Thiago Pires <thiago.serip@gmail.com>
2025-01-23 15:46:07 -0300

    .

--- streamlit/utils/functions.py
+++ streamlit/utils/functions.py
@@ -180,11 +180,11 @@ def get_filter_lay_0x1(df):
     return (
         (df["Odd_H_FT"] < df["Odd_A_FT"]) &
         (df["Odd_H_FT"] > 1.5) &
-        ((df["Odd_H_FT"] < 2.45) | (df["Odd_H_FT"] > 2.55)) &
+        # ((df["Odd_H_FT"] < 2.45) | (df["Odd_H_FT"] > 2.55)) &
         (df["XG_Away_Pre"] > 1.12) &
         (df["XG_Home_Pre"] > df["XG_Away_Pre"]) &
         (df["Odd_BTTS_Yes"] >= 1.5) & (df["Odd_BTTS_Yes"] < 2) &
-        (df["Odd_Over25_FT"] >= 1.65) & (df["Odd_Over25_FT"] < 2) &
+        (df["Odd_Over25_FT"] >= 1.6) & (df["Odd_Over25_FT"] < 2) &
         (df['League'].isin([
             'Belgium Pro League',
             'England Championship',
