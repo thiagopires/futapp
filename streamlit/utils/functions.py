e53bfd76b282af3a4f03d647abc64d3bf8502e4e
Thiago Pires <thiago.serip@gmail.com>
2025-01-28 15:45:04 -0300

    .

--- streamlit/utils/functions.py
+++ streamlit/utils/functions.py
@@ -52,7 +52,7 @@ def get_filter_over(df):
     return (
         (df["XG_Home_Pre"] >= 1.3) &
         (df["XG_Away_Pre"] >= 1.3) &
-        (df["Odd_Over25_FT"] >= 1.45) &
+        (df["Odd_Over25_FT"] >= 1.4) &
         (df["Odd_H_FT"] < df["Odd_A_FT"]) # &
 
         # (
