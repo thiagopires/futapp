22a0b8f5b01a000c52fc28d9b18a606ad831e939
Thiago Pires <thiago.serip@gmail.com>
2025-01-27 23:22:42 -0300

    .

--- streamlit/utils/functions.py
+++ streamlit/utils/functions.py
@@ -215,17 +215,16 @@ def get_filter_lay_visitante_zebra(df):
         (df["XG_Home_Pre"] > df["XG_Away_Pre"]) &
         (df["XG_Total_Pre"] >= 1.7) &
         (df["XG_Away_Pre"] <= 1.25) 
-        # &
-        # (df['League'].isin([
-        #     'Turkey Süper Lig',
-        #     'England EFL League One',
-        #     'Italy Serie B',
-        #     'Italy Serie A',
-        #     'England Premier League',
-        #     'France Ligue 1',
-        #     'Spain Segunda División',
-        #     'Germany 2. Bundesliga'
-        # ]))
+        &
+        (df['League'].isin([
+            'Turkey Süper Lig',
+            'England EFL League One',
+            'Italy Serie B',
+            'Spain Segunda División',
+            'Italy Serie A',
+            'France Ligue 1',
+            'England Premier League'
+        ]))
     )
 
 def get_filter_back_empate(df):
