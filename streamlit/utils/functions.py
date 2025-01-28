a84c1a25dbd66bbd9e858b28eaeda9766105cf47
Thiago Pires <thiago.serip@gmail.com>
2025-01-28 00:11:51 -0300

    .

--- streamlit/utils/functions.py
+++ streamlit/utils/functions.py
@@ -53,7 +53,7 @@ def get_filter_over(df):
         # (df["XG_Home_Pre"] >= 1.3) &
         # (df["XG_Away_Pre"] >= 1.1) &
         (df["Diff_XG_Home_Away_Pre"] < 0.7) & (df["Diff_XG_Home_Away_Pre"] > 0) &
-        (df["Odd_Over25_FT"] >= 1.55) 
+        (df["Odd_Over25_FT"] >= 1.5) 
         &
         (df['League'].isin([
             'Portugal Liga NOS',
