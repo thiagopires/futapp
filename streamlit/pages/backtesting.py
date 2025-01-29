0c6fadaa3e85e91f2296c41fba13ddd9354bac86
Thiago Pires <thiago.serip@gmail.com>
2025-01-29 13:39:53 -0300

    .

--- streamlit/pages/backtesting.py
+++ streamlit/pages/backtesting.py
@@ -140,7 +140,8 @@ def main_page():
             'Over 2.5 FT',
             'Under 2.5 FT',
             'BTTS Sim',
-            'Lay 0x1 (até 80min)'
+            'Lay 0x1 (até 80min)',
+            'Lay 0x2 (até 80min)',
             # 'BTTS Não'
         ])
 
