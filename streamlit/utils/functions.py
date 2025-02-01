21b91a0e1d60f29db823b856da17564b2e15a8f2
Thiago <thiago.serip@gmail.com>
2025-02-01 00:34:40 -0300

    Update functions.py

--- streamlit/utils/functions.py
+++ streamlit/utils/functions.py
@@ -295,7 +295,7 @@ def get_filter_back_home(df):
             ((df['League'] == 'ARGENTINA - PRIMERA DIVISIÓN') & (df["Probabilidade_H_FT"].between(0.42, 0.51))) |
             ((df['League'] == 'BELGIUM - PRO LEAGUE') & (df["Probabilidade_H_FT"].between(0.52, 0.61))) |
             ((df['League'] == 'BRAZIL - SERIE A') & (df["Probabilidade_H_FT"].between(0.62, 0.71))) |
-            ((df['League'] == 'BRAZIL - SERIE B' & (df["Probabilidade_H_FT"].between(0.62, 0.71))) |
+            ((df['League'] == 'BRAZIL - SERIE B') & (df["Probabilidade_H_FT"].between(0.62, 0.71))) |
             ((df['League'] == 'GERMANY - BUNDESLIGA') & (df["Probabilidade_H_FT"].between(0.42, 0.51))) |
             ((df['League'] == 'JAPAN - J2 LEAGUE') & (df["Probabilidade_H_FT"].between(0.52, 0.81))) |
             ((df['League'] == 'NETHERLANDS - EERSTE DIVISIE') & (df["Probabilidade_H_FT"].between(0.62, 0.71))) |
