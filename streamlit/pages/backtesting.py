b8dc56da4c96982033d7060c8973759975839ce2
Thiago Pires <thiago.serip@gmail.com>
2025-02-06 17:24:31 -0300

    .

--- streamlit/pages/backtesting.py
+++ streamlit/pages/backtesting.py
@@ -309,11 +309,7 @@ def main_page():
             df_hist.loc[(df_hist["Resultado_80"] != '0-1'), "Status_Metodo"] = "GREEN"
             df_hist['Profit'] = 0
         if metodo == 'Lay 1x1':
-            # df_hist.loc[((df_hist["Resultado_FT"] != '1-1') & (df_hist["Resultado_80"] != '1-1')), "Status_Metodo"] = "GREEN"
-            df_hist.loc[
-                (df_hist["Resultado_60"] != '1-1') &
-                (df_hist["Primeiro_Gol_Minuto"] < 45) &
-                (df_hist["Primeiro_Gol_Marcador"] == "Home"), "Status_Metodo"] = "GREEN"
+            df_hist.loc[df_hist["Resultado_60"] != '1-1', "Status_Metodo"] = "GREEN"
             df_hist['Profit'] = 0
         if metodo == 'Lay 0x2':
             df_hist.loc[df_hist["Resultado_80"] != '0-2', "Status_Metodo"] = "GREEN"
