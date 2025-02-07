cf81d3cc372a20c2e966da4fb4e5a7c01e7dd231
Thiago Pires <thiago.serip@gmail.com>
2025-02-07 20:43:08 -0300

    .

--- streamlit/pages/analise-home.py
+++ streamlit/pages/analise-home.py
@@ -12,7 +12,8 @@ def main_page():
     if st.secrets['ENV'] == 'dev':
         st.info("Ambiente de Desenvolvimento. Branch: dev")
 
-    st.title("⚽ Análise Home")
+    st.title("Futapp v0.1")
+    st.header("⚽ Análise Home")
 
     # Init
 
