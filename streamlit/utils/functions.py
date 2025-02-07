af3b3b4daa578fe1ae760ecfc655dd7b2e45f07f
Thiago Pires <thiago.serip@gmail.com>
2025-02-07 20:46:06 -0300

    .

--- streamlit/utils/functions.py
+++ streamlit/utils/functions.py
@@ -41,7 +41,8 @@ def login_page():
     
     display_sidebar('none')
 
-    st.title("Login")
+    st.title("Bem vindo ao Futapp.")
+    st.header("Login")
     email = st.text_input("Digite seu e-mail cadastrado para acessar", placeholder="email@example.com")
     if st.button("Entrar"):
         if st.session_state["logged_in"] == True:
