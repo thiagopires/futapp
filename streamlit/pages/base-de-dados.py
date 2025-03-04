04aaf0e62325951bb5ca411292aaa4b3ebe9fe30
Thiago Pires <thiago.serip@gmail.com>
2025-03-04 11:18:19 -0300

    .

--- streamlit/pages/base-de-dados.py
+++ streamlit/pages/base-de-dados.py
@@ -33,12 +33,12 @@ def main_page():
     if not (not selected_leagues or "Todas as Ligas" in selected_leagues):
         df_hist = df_hist[df_hist['League'].isin(selected_leagues)]
 
-    seasons = sorted(df_hist['Season'].unique())
-    seasons.insert(0, 'Todas as Temporadas')
-    selected_seasons = st.multiselect("Filtrar por Temporada", seasons, [seasons[0]])
+    # seasons = sorted(df_hist['Season'].unique())
+    # seasons.insert(0, 'Todas as Temporadas')
+    # selected_seasons = st.multiselect("Filtrar por Temporada", seasons, [seasons[0]])
 
-    if not (not selected_seasons or "Todas as Temporadas" in selected_seasons):
-        df_hist = df_hist[df_hist['Season'].isin(selected_seasons)]
+    # if not (not selected_seasons or "Todas as Temporadas" in selected_seasons):
+    #     df_hist = df_hist[df_hist['Season'].isin(selected_seasons)]
 
     if st.button("Carregar dados"):
         print_dataframe(df_hist)
