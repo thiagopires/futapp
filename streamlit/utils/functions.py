e0774a590f8a45416508acd26e6d38ac92279559
Thiago Pires <thiago.serip@gmail.com>
2025-03-06 08:53:29 -0300

    .

--- streamlit/utils/functions.py
+++ streamlit/utils/functions.py
@@ -139,9 +139,9 @@ def load_daymatches(dt, source):
             df['XG_Home_Pre'] = 1
             df['XG_Away_Pre'] = 1
             df["Diff_XG_Home_Away_Pre"] = 1
-            df["Odd_DC_1X"] = (df["Odd_H_FT_Lay"] * df["Odd_D_FT_Lay"]) / (df["Odd_H_FT_Lay"] + df["Odd_D_FT_Lay"] - 1)
-            df["Odd_DC_12"] = (df["Odd_H_FT_Lay"] * df["Odd_A_FT_Lay"]) / (df["Odd_H_FT_Lay"] + df["Odd_A_FT_Lay"] - 1)
-            df["Odd_DC_X2"] = (df["Odd_D_FT_Lay"] * df["Odd_A_FT_Lay"]) / (df["Odd_D_FT_Lay"] + df["Odd_A_FT_Lay"] - 1)
+            df["Odd_DC_1X"] = (df["Odd_H_FT"] * df["Odd_D_FT"]) / (df["Odd_H_FT"] + df["Odd_D_FT"] - 1)
+            df["Odd_DC_12"] = (df["Odd_H_FT"] * df["Odd_A_FT"]) / (df["Odd_H_FT"] + df["Odd_A_FT"] - 1)
+            df["Odd_DC_X2"] = (df["Odd_D_FT"] * df["Odd_A_FT"]) / (df["Odd_D_FT"] + df["Odd_A_FT"] - 1)
         elif source == 'FootyStats':
             df["Diff_XG_Home_Away_Pre"] = df['XG_Home_Pre'] - df['XG_Away_Pre']
 
@@ -249,9 +249,9 @@ def load_histmatches(source):
             df['XG_Home_Pre'] = 1
             df['XG_Away_Pre'] = 1
             df["Diff_XG_Home_Away_Pre"] = 1
-            df["Odd_DC_1X"] = (df["Odd_H_FT_Lay"] * df["Odd_D_FT_Lay"]) / (df["Odd_H_FT_Lay"] + df["Odd_D_FT_Lay"] - 1)
-            df["Odd_DC_12"] = (df["Odd_H_FT_Lay"] * df["Odd_A_FT_Lay"]) / (df["Odd_H_FT_Lay"] + df["Odd_A_FT_Lay"] - 1)
-            df["Odd_DC_X2"] = (df["Odd_D_FT_Lay"] * df["Odd_A_FT_Lay"]) / (df["Odd_D_FT_Lay"] + df["Odd_A_FT_Lay"] - 1)
+            df["Odd_DC_1X"] = (df["Odd_H_FT"] * df["Odd_D_FT"]) / (df["Odd_H_FT"] + df["Odd_D_FT"] - 1)
+            df["Odd_DC_12"] = (df["Odd_H_FT"] * df["Odd_A_FT"]) / (df["Odd_H_FT"] + df["Odd_A_FT"] - 1)
+            df["Odd_DC_X2"] = (df["Odd_D_FT"] * df["Odd_A_FT"]) / (df["Odd_D_FT"] + df["Odd_A_FT"] - 1)
             df['PPG_Home_Pre'] = 0
             df['PPG_Away_Pre'] = 0
         elif source == 'FootyStats':
