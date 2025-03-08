0d8ac1ee1d9061fe87f5051724dc60f1ab4bc250
Thiago Pires <thiago.serip@gmail.com>
2025-03-07 23:05:19 -0300

    .

--- streamlit/utils/filters.py
+++ streamlit/utils/filters.py
@@ -621,7 +621,7 @@ def get_filter_lay_visitante_v2(df):
     
     return (
         (df["Odd_H_FT"].between(1.3, 2.5)) &
-        (df["Odd_H_FT"] < df["Odd_A_FT"]) &
+        # (df["Odd_H_FT"] < df["Odd_A_FT"]) &
         (
 
             ((df['League'] == 'AUSTRALIA - A-LEAGUE') 
