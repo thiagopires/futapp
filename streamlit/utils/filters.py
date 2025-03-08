befac2b6a2f2a3aa210bf8537e9413cfab8e92ff
Thiago Pires <thiago.serip@gmail.com>
2025-03-08 16:22:54 -0300

    .

--- streamlit/utils/filters.py
+++ streamlit/utils/filters.py
@@ -647,10 +647,9 @@ def get_filter_lay_visitante_v2(df):
                 & (df["Probabilidade_A_FT"].between(0.32, 0.41))
                 & (df["CV_HDA_FT"].between(0.15, 0.24))) |
 
-
-            ((df['League'] == 'ENGLAND - CHAMPIONSHIP') 
-                & (df["Probabilidade_A_FT"].between(0.12, 0.21))
-                & (df["CV_HDA_FT"].between(0.3, 0.34))) |
+            # ((df['League'] == 'ENGLAND - CHAMPIONSHIP') 
+            #     & (df["Probabilidade_A_FT"].between(0.12, 0.21))
+            #     & (df["CV_HDA_FT"].between(0.3, 0.34))) |
 
             ((df['League'] == 'ENGLAND - CHAMPIONSHIP') 
                 & (df["Probabilidade_A_FT"].between(0.22, 0.31))
@@ -659,7 +658,6 @@ def get_filter_lay_visitante_v2(df):
             ((df['League'] == 'ENGLAND - CHAMPIONSHIP') 
                 & (df["Probabilidade_A_FT"].between(0.32, 0.41))
                 & (df["CV_HDA_FT"].between(0.15, 0.24))) |
-
 
             ((df['League'] == 'ENGLAND - PREMIER LEAGUE') 
                 & (df["Probabilidade_A_FT"].between(0.22, 0.31))
