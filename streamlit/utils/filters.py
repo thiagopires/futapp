b0c4440b8aab1676f7653b044980d1183dd636bc
Thiago Pires <thiago.serip@gmail.com>
2025-03-10 13:38:09 -0300

    .

--- streamlit/utils/filters.py
+++ streamlit/utils/filters.py
@@ -737,7 +737,7 @@ def get_filter_lay_visitante_v2(df):
 
 
     return (
-        df['Odd_A_FT'].between(2.4, 5.3) & (
+        df['Odd_A_FT'].between(2.4, 5.4) & (
             ((df['League'] == 'ARGENTINA - PRIMERA DIVISIÓN') & (df['FX_Probabilidade_A'] == '0.39-0.48') & (df['FX_CV_HDA'] == '0.05-0.10')) |
             ((df['League'] == 'ARGENTINA - PRIMERA DIVISIÓN') & (df['FX_Probabilidade_A'] == '0.39-0.48') & (df['FX_CV_HDA'] == '0.15-0.20')) |
             ((df['League'] == 'ARGENTINA - PRIMERA DIVISIÓN') & (df['FX_Probabilidade_A'] == '0.39-0.48') & (df['FX_CV_HDA'] == '0.20-0.25')) |
