dcc2ea715b58985efa343e3e97880351c42914c0
Thiago Pires <thiago.serip@gmail.com>
2025-03-12 13:31:01 -0300

    .

--- streamlit/pages/backtesting.py
+++ streamlit/pages/backtesting.py
@@ -135,10 +135,10 @@ def main_page(fonte_dados):
             total_reds = len(df_hist[(df_hist['Status_Metodo'] == 'RED')])
             total_voids = len(df_hist[(df_hist['Status_Metodo'] == 'VOID')])
             winrate = round(total_greens / total_jogos * 100, 2)
-            profit_acumulado = f"{str(round(df_hist['Profit'].sum(), 2))} unidades"
+            profit_acumulado = f"{str(round(df_hist['Profit'].sum(), 2))} unidades,"
             
             str_voids = f'Voids: {total_voids}' if total_voids > 0 else ''
-            st.write(f"Jogos: {total_jogos}, Greens: {total_greens}, Reds: {total_reds}, {str_voids}, Winrate: {winrate}%, Profit Acumulado Líquido: {profit_acumulado}, Comissão: 2.8%, Odd Média: {odd_media}")
+            st.write(f"Jogos: {total_jogos}, Greens: {total_greens}, Reds: {total_reds}, {str_voids} Winrate: {winrate}%, Profit Acumulado Líquido: {profit_acumulado}, Comissão: 2.8%, Odd Média: {odd_media}")
 
             daily_profit = df_hist.groupby("Date")["Profit"].sum().reset_index()
             daily_profit["Cumulative_Profit"] = daily_profit["Profit"].cumsum()  
