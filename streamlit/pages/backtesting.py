bc4412627c83b8c99ac936ffdfe0ed084fb87245
Thiago Pires <thiago.serip@gmail.com>
2025-03-12 18:23:14 -0300

    .

--- streamlit/pages/backtesting.py
+++ streamlit/pages/backtesting.py
@@ -206,7 +206,7 @@ def main_page(fonte_dados):
                 print_dataframe(report)
             with col2:
                 st.write("**Resultado por FX (Prob, CV) do MO**")
-                report = df_hist.groupby(["League", "FX_Probabilidade_A", "FX_CV_HDA", "Status_Metodo"]).size().unstack(fill_value=0).reset_index()
+                report = df_hist.groupby(["League", "FX_Probabilidade_A", "FX_CV_HDA", "Status_Metodo"], observed=True).size().unstack(fill_value=0).reset_index()
                 report = report[report['GREEN'] + report['RED'] > 0]
                 report['Winrate'] = round((report['GREEN'] / (report['GREEN'] + report['RED'])) * 100, 2)
                 # report["Cumulative_Profit"] = report["Profit"].cumsum()
