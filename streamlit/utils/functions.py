e4f708f47b6981ea60bd3d38bced32698992fc7b
Thiago Pires <thiago.serip@gmail.com>
2025-03-25 12:59:12 -0300

    .

--- streamlit/utils/functions.py
+++ streamlit/utils/functions.py
@@ -98,8 +98,9 @@ def load_daymatches(dt, source):
         if source == 'Betfair':            
             # df = pd.read_csv(load_content_api_github(f"Jogos_do_Dia/Betfair/Jogos_do_Dia_Betfair_Back_Lay_{dt}.csv"))
 
-            client = MongoClient(st.secrets["mongodb_connectionString"])
-            db = client[st.secrets["mongodb_database"]]
+            connectionString, mongodb_database = st.secrets["mongodb"]
+            client = MongoClient(connectionString)
+            db = client[mongodb_database]
             collection = db['bf_jogos_do_dia']
 
             data = list(collection.find({"Date": dt}))
