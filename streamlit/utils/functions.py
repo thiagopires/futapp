e3e2c27f23db54c08393a30743e0b7575bd1975f
Thiago Pires <thiago.serip@gmail.com>
2025-05-09 15:11:44 -0300

    .

--- streamlit/utils/functions.py
+++ streamlit/utils/functions.py
@@ -162,7 +162,11 @@ def load_daymatches(dt, source):
             df["Odd_DC_1X"] = round(1 / (1 / df["Odd_H_FT"] + 1 / df["Odd_D_FT"]),2)
             df["Odd_DC_12"] = round(1 / (1 / df["Odd_H_FT"] + 1 / df["Odd_A_FT"]),2)
             df["Odd_DC_X2"] = round(1 / (1 / df["Odd_D_FT"] + 1 / df["Odd_A_FT"]),2)
-            df["Resultado"] = f"{str(df['Goals_H_FT'])}-{str(df['Goals_A_FT'])}"
+            if {"Goals_H_FT", "Goals_A_FT"}.issubset(df.columns):
+                df["Resultado"] = df.apply(lambda row: f"{row['Goals_H_FT']}-{row['Goals_A_FT']}", axis=1)
+            else:
+                df["Resultado"] = "N/A"
+
         elif source == 'FootyStats':
             df["Diff_XG_Home_Away_Pre"] = df['XG_Home_Pre'] - df['XG_Away_Pre']
             df["Odd_CS_0x1_Lay"] = 0
