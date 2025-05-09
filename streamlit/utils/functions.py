dedc6044b5989004297624838ee007057e70f84f
Thiago Pires <thiago.serip@gmail.com>
2025-05-09 15:26:12 -0300

    .

--- streamlit/utils/functions.py
+++ streamlit/utils/functions.py
@@ -163,7 +163,7 @@ def load_daymatches(dt, source):
             df["Odd_DC_12"] = round(1 / (1 / df["Odd_H_FT"] + 1 / df["Odd_A_FT"]),2)
             df["Odd_DC_X2"] = round(1 / (1 / df["Odd_D_FT"] + 1 / df["Odd_A_FT"]),2)
             if {"Goals_H_FT", "Goals_A_FT"}.issubset(df.columns):
-                df["Resultado"] = df.apply(lambda row: f"{str(int(row['Goals_H_FT']))}-{str(int(row['Goals_H_FT']))}", axis=1)
+                df["Resultado"] = df.apply(lambda row: f"{str(int(row['Goals_H_FT']))}-{str(int(row['Goals_A_FT']))}", axis=1)
             else:
                 df["Resultado"] = "N/A"
 
