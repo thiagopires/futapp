bef44ab27f9436b6a59d358a8ef13fda881345df
Thiago Pires <thiago.serip@gmail.com>
2025-08-23 17:57:06 -0300

    .

--- streamlit/utils/functions.py
+++ streamlit/utils/functions.py
@@ -163,14 +163,15 @@ def load_daymatches(dt, source):
             df["Odd_DC_12"] = round(1 / (1 / df["Odd_H_FT"] + 1 / df["Odd_A_FT"]),2)
             df["Odd_DC_X2"] = round(1 / (1 / df["Odd_D_FT"] + 1 / df["Odd_A_FT"]),2)
             if {"Goals_H_FT", "Goals_A_FT"}.issubset(df.columns):
-                def format_result(row):
-                    if pd.notnull(row["Goals_H_FT"]) and pd.notnull(row["Goals_A_FT"]):
-                        return f"{int(row['Goals_H_FT'])}-{int(row['Goals_A_FT'])}"
-                    else:
-                        return "N/A"
-                df["Resultado"] = df.apply(format_result, axis=1)
+                df["Resultado"] = df.apply(
+                    lambda r: f"{int(r['Goals_H_FT'])}-{int(r['Goals_A_FT'])}"
+                    if pd.notnull(r["Goals_H_FT"]) and pd.notnull(r["Goals_A_FT"])
+                    else "N/A",
+                    axis=1
+                )
             else:
                 df["Resultado"] = "N/A"
+
 
         elif source == 'FootyStats':
             df["Diff_XG_Home_Away_Pre"] = df['XG_Home_Pre'] - df['XG_Away_Pre']
