a635a80fcf6923be9356c96b59cf10c64221f74c
Thiago Pires <thiago.serip@gmail.com>
2025-08-28 00:19:28 -0300

    .

--- streamlit/pages/jogos_sem_resultado.py
+++ streamlit/pages/jogos_sem_resultado.py
@@ -21,7 +21,7 @@ def main_page(fonte_dados):
 
     else:
         # Dataframe
-        df_matches_columns = ['Event_ID','League','Filtro','Date','Time','Home','Away','Resultado','Goals_H_Minutes','Goals_A_Minutes','Odd_H_FT','Odd_D_FT','Odd_A_FT','Odd_Over25_FT','Odd_Under25_FT','Odd_BTTS_Yes','Odd_BTTS_No','Odd_CS_0x1_Lay','Odd_CS_0x2_Lay','Odd_CS_0x3_Lay','XG_Total_Pre','XG_Home_Pre','XG_Away_Pre','Odd_DC_1X','Odd_DC_12','Odd_DC_X2']
+        df_matches_columns = ['Event_ID','League','Filtro','Home','Away','Resultado','Goals_H_Minutes','Goals_A_Minutes']
        
         match_selected = st.dataframe(
             df_matches[df_matches_columns]
