ea9768120b5ee3194d6332057b09de642d089199
Thiago Pires <thiago.serip@gmail.com>
2025-09-10 23:23:02 -0300

    .

--- streamlit/utils/functions.py
+++ streamlit/utils/functions.py
@@ -122,11 +122,9 @@ def load_daymatches(dt, source):
             
             filter = {"Date": f"{dt}"} if dt is not None else {}
             data = list(collection.find(filter))
-            df = pd.DataFrame(data)
+            df = pd.DataFrame(data).sort_values(['Date', 'Time'])
 
-            if not df.empty:
-                df = df.sort_values(['Date', 'Time'])
-                df = transform_df_betfair(df)
+            df = transform_df_betfair(df)
 
         elif source == 'FootyStats':
             df = pd.read_csv(f"https://github.com/futpythontrader/YouTube/blob/main/Jogos_do_Dia/FootyStats/Jogos_do_Dia_FootyStats_{dt}.csv?raw=true")
@@ -190,6 +188,8 @@ def load_daymatches(dt, source):
     except urllib.error.HTTPError as e:
         return pd.DataFrame()
     except pd.errors.EmptyDataError as e:
+        return pd.DataFrame()
+    except KeyError as e:
         return pd.DataFrame()
 
 @st.cache_data
