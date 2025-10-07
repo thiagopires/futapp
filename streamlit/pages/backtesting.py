6d1cb2d6d83894679432e58b0c0785a436823278
Thiago Pires <thiago.serip@gmail.com>
2025-10-07 11:18:55 -0300

    .

--- streamlit/pages/backtesting.py
+++ streamlit/pages/backtesting.py
@@ -4,7 +4,7 @@ from utils.filters import *
 from datetime import date
 import plotly.express as px
 
-def generate_backtesting(metodo):
+def generate_backtesting(df_hist, metodo):
 
     df_hist, odd_media = get_result_filtro_pronto(df_hist, metodo)
 
@@ -246,27 +246,27 @@ def main_page(fonte_dados):
 
         tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9, tab10, tab11 = st.tabs(metodos_tabs)
         with tab1:
-            generate_backtesting(tab1)
+            generate_backtesting(df_hist, tab1)
         with tab2:
-            generate_backtesting(tab2)
+            generate_backtesting(df_hist, tab2)
         with tab3:
-            generate_backtesting(tab3)
+            generate_backtesting(df_hist, tab3)
         with tab4:
-            generate_backtesting(tab4)
+            generate_backtesting(df_hist, tab4)
         with tab5:
-            generate_backtesting(tab5)
+            generate_backtesting(df_hist, tab5)
         with tab6:
-            generate_backtesting(tab6)
+            generate_backtesting(df_hist, tab6)
         with tab7:
-            generate_backtesting(tab7)
+            generate_backtesting(df_hist, tab7)
         with tab8:
-            generate_backtesting(tab8)
+            generate_backtesting(df_hist, tab8)
         with tab9:
-            generate_backtesting(tab9)
+            generate_backtesting(df_hist, tab9)
         with tab10:
-            generate_backtesting(tab10)
+            generate_backtesting(df_hist, tab10)
         with tab11:
-            generate_backtesting(tab11)
+            generate_backtesting(df_hist, tab11)
 
 # if "logged_in" not in st.session_state:
 #     st.session_state["logged_in"] = False
