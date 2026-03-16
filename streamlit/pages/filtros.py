b7d3660d8a5cd419b1f58498e0fe6eddfa429518
Thiago Pires <thiago.serip@gmail.com>
2026-03-16 18:04:47 -0300

    .

--- streamlit/pages/filtros.py
+++ streamlit/pages/filtros.py
@@ -25,6 +25,7 @@ def main_page(fonte_dados):
     else:
 
         for fp in filtros_prontos[fonte_dados].pop(0):
+            print(fp)
 
             df_matches, condicao, metodo = get_details_filtro_pronto(df_matches, None, None, fp)
 
