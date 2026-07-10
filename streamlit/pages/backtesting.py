5fcca1c1beeb17036cc7831d2bfb4dcdddcc6fb7
Thiago Pires <thiago.serip@gmail.com>
2026-07-10 17:42:56 -0300

    .

--- streamlit/pages/backtesting.py
+++ streamlit/pages/backtesting.py
@@ -111,7 +111,7 @@ def display_reports(df_hist):
 
 def display_details(df_hist, stats):
     # Exibição dos jogos detalhados
-    df_columns = ['League','Rodada','Date','Time','Home','Away','Resultado','Goals_H_Minutes','Goals_A_Minutes','Odd_H_FT','Odd_D_FT','Odd_A_FT','Odd_CS_0x1_Lay','Odd_CS_1x1','Odd_CS_0x2_Lay','Odd_CS_0x3_Lay','Odd_CS_Goleada_A','Odd_Over05_FT','Odd_Over25_FT','Odd_Under05_FT','Odd_Under15_FT','Odd_Under25_FT','Odd_BTTS_Yes','Odd_BTTS_No','Odd_DC_1X','Odd_DC_12','Odd_DC_X2','XG_Total_Pre','XG_Home_Pre','XG_Away_Pre','Diff_XG_Home_Away_Pre','PPG_Home_Pre','PPG_Away_Pre','Primeiro_Gol','Status_Metodo','Profit','Probabilidade_H_FT','Probabilidade_D_FT','Probabilidade_A_FT','CV_HDA_FT']
+    df_columns = ['League','Rodada','Date','Time','Home','Away','Resultado','Goals_H_Minutes','Goals_A_Minutes','Odd_H_FT','Odd_D_FT','Odd_A_FT','Odd_CS_0x1_Lay','Odd_CS_1x1','Odd_CS_0x2_Lay','Odd_CS_0x3_Lay','Odd_CS_Goleada_H','Odd_CS_Goleada_A','Odd_Over05_FT','Odd_Over25_FT','Odd_Under05_FT','Odd_Under15_FT','Odd_Under25_FT','Odd_BTTS_Yes','Odd_BTTS_No','XG_Total_Pre','XG_Home_Pre','XG_Away_Pre','Diff_XG_Home_Away_Pre','PPG_Home_Pre','PPG_Away_Pre','Primeiro_Gol','Status_Metodo','Profit','Probabilidade_H_FT','Probabilidade_D_FT','Probabilidade_A_FT','CV_HDA_FT']
     
     # Garantir que apenas colunas existentes sejam selecionadas
     display_cols = [col for col in df_columns if col in df_hist.columns]
